77ee6e58a84d8f806a79a67d4c060f0e750da8e4
Abhilash <abhil9410@gmail.com>
2021-11-30 12:16:00 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularapp';
+  errorName:boolean=false;
+  errorGender:boolean=false;
+  errorCountry:boolean=false;
+  errorCity:boolean=false;
+  errorAge:boolean=false;
+  iserror:boolean=false;
+
+  userDetail = new FormGroup({
+    username: new FormControl(''),
+    Gender: new FormControl(''),
+    Country: new FormControl(''),
+    City: new FormControl(''),
+    age: new FormControl(''),
+  })
+
+  validateData() {
+    console.log(this.userDetail.value.username);
+    console.log(this.userDetail.value);
+    var regName = /^[a-zA-Z]{3,50}$/;
+    var name = this.userDetail.value.username;
+    if (!regName.test(name)) {
+      this.errorName=true;
+    } else {
+      alert('Valid name given.');
+    }
+    if (!this.userDetail.value.Gender) {
+      this.errorGender=true;
+      this.iserror=true;
+    }
+    if (!this.userDetail.value.Country) {
+      this.errorCountry=true;
+      this.iserror=true;
+
+    }
+    if (!this.userDetail.value.Country) {
+      this.errorCity=true;
+      this.iserror=true;
+    }
+
+    if(!this.userDetail.value.age && this.userDetail.value.Country)
+    {
+       if(this.userDetail.value.Country == "india" || this.userDetail.value.Country == "US")
+       {
+         this.errorAge=true;
+         this.iserror=true;
+       }
+    }
+
+
+    if(!this.iserror)
+    {
+       window.location.reload();
+    }
+
+
+  }
+
 }
